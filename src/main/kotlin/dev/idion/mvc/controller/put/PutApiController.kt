a9779581ea4a7c154fdac446e7fc8fec1aa6afc9
Dion <ksundong@gmail.com>
2020-11-20 13:24:37 +0900

    feat: 중첩 Json 응답 구현
    추가로 kotlin apply 확장함수를 활용

--- src/main/kotlin/dev/idion/mvc/controller/put/PutApiController.kt
+++ src/main/kotlin/dev/idion/mvc/controller/put/PutApiController.kt
@@ -1,6 +1,8 @@
 package dev.idion.mvc.controller.put
 
+import dev.idion.mvc.model.http.Result
 import dev.idion.mvc.model.http.UserRequest
+import dev.idion.mvc.model.http.UserResponse
 import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -18,5 +20,35 @@ class PutApiController {
     @PutMapping("/put/object")
     fun putMappingObject(@RequestBody userRequest: UserRequest): UserRequest {
         return userRequest
+    }
+
+    @PutMapping("/put/response-object")
+    fun putMappingResponseObject(): UserResponse {
+        return UserResponse().apply {
+            this.result = Result().apply {
+                this.resultCode = "OK"
+                this.resultMessage = "성공"
+            }
+        }.apply {
+            this.description = "~~~~~~~~~~~~~~~~~~~~~~"
+        }.apply {
+            val userList = mutableListOf<UserRequest>()
+            userList.add(UserRequest().apply {
+                this.name = "a"
+                this.age = 10
+                this.email = "a@gmail.com"
+                this.address = "a address"
+                this.phoneNumber = "010-1234-5666"
+            })
+            userList.add(UserRequest().apply {
+                this.name = "b"
+                this.age = 15
+                this.email = "b@gmail.com"
+                this.address = "b address"
+                this.phoneNumber = "010-1234-5663"
+            })
+
+            this.userRequests = userList
+        }
     }
 }
