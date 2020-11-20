ac298558eac18966803f1eaf555e8789e210a25a
Dion <ksundong@gmail.com>
2020-11-20 12:49:52 +0900

    feat: put method object api 구현

--- src/main/kotlin/dev/idion/mvc/controller/put/PutApiController.kt
+++ src/main/kotlin/dev/idion/mvc/controller/put/PutApiController.kt
@@ -1,6 +1,8 @@
 package dev.idion.mvc.controller.put
 
+import dev.idion.mvc.model.http.UserRequest
 import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -11,5 +13,10 @@ class PutApiController {
     @PutMapping("/put")
     fun putMapping(): String {
         return "put"
+    }
+
+    @PutMapping("/put/object")
+    fun putMappingObject(@RequestBody userRequest: UserRequest): UserRequest {
+        return userRequest
     }
 }
